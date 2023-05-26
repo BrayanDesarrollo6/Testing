2fcfe3dde62071c2ea579d394ffc637859d6b8a0
BrayanDesarrollo6 <desarrollo6@hq5.com.co>
2023-05-26 11:22:01 -0500

    Express con post y get además de config node y corregido el puerto

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4001;
+const port = 6000;
 const cors = require('cors');
 
 // Middleware para parsear el cuerpo de las solicitudes POST

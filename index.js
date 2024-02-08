f483313445ab88e70a7d14474f8b5f5cee7deb3b
desarrollo4hq5 <desarrollo4@hq5.com.co>
2024-02-08 17:46:17 -0500

    Implementacion con cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = 6000;
 
+app.use(cors());
+
 app.use(express.json());
 
 app.post('/api/test', (req, res) => {
-
     const data = req.body;
     console.log(data);
     res.status(200).json({ message: 'Solicitud POST recibida con éxito' });
